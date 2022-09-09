d03bb7d7a34e9c25b5060686684e770cf3eb74e6
Shahriar Rumel <rumelshahriarhere@gmail.com>
2022-09-10 02:36:37 +0600

    Installation and Default Location Customization

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+   
+   
+   import { StyleSheet, View } from 'react-native';
+    import MapView from 'react-native-maps';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Google Map Integration!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    export default function App() {
+      return (
+        <View style={styles.container}>
+          <MapView style={styles.map} initialRegion={{
+            latitude:23.7338,
+            longitude:90.3929,
+            latitudeDelta:0.0012,
+            longitudeDelta:0.0021
+          }}/>
+        </View>
+      );
+    }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      map: {
+        width: '100%',
+        height: '100%',
+      },
+    });
