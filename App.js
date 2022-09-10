081e88dc924cc20518b774d3d1213aa31255ceaf
Shahriar Rumel <rumelshahriarhere@gmail.com>
2022-09-10 23:56:54 +0600

    Marker Functionalities Completed

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
    
    
-   import { StyleSheet, View } from 'react-native';
-    import MapView from 'react-native-maps';
+    import { useState } from 'react';
+    import { StyleSheet, View,Text } from 'react-native';
+    import MapView, { Callout, Marker } from 'react-native-maps';
 
     export default function App() {
+
+      const [marker,setMarker] = useState({
+        latitude:23.7338,
+        longitude:90.3929
+      });
+
       return (
         <View style={styles.container}>
           <MapView style={styles.map} initialRegion={{
@@ -11,7 +18,25 @@
             longitude:90.3929,
             latitudeDelta:0.0012,
             longitudeDelta:0.0021
-          }}/>
+          }}>
+            <Marker 
+              coordinate = {marker} 
+              draggable={true}
+              onDragStart={(e)=>{
+                console.log("Drag Start",e.nativeEvent.coordinate)
+              }}
+              onDragEnd={(e)=>{
+               setMarker({
+                latitude:e.nativeEvent.coordinate.latitude,
+                longitude:e.nativeEvent.coordinate.longitude
+               })
+              }}
+            >
+                <Callout>
+                  <Text>It's a tooltip</Text>
+                </Callout>
+             </Marker>
+          </MapView>
         </View>
       );
     }
